da25ea50b589e472111c8c95d2416684eac13dc0
astsu <astsu.dev@protonmail.com>
2023-04-17 20:47:12 +0300

    chore(lint): Add prettier and eslint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+  },
+  root: true,
+};
