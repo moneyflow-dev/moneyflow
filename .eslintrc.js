06a35cb6859970d4ae984194c90192566a3640ca
astsu <astsu.dev@protonmail.com>
2023-04-17 21:18:30 +0300

    chore(lint): Add react plugins to eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,12 +2,15 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
     "prettier",
   ],
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint", "prettier"],
+  plugins: ["@typescript-eslint", "react", "prettier"],
   rules: {
     "prettier/prettier": "error",
+    "react/react-in-jsx-scope": "off",
   },
   root: true,
 };
