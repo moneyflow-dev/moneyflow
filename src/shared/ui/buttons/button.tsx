2c419f59007c799bedc88f49c5737ff117038c82
astsu <astsu.dev@protonmail.com>
2023-04-20 10:28:14 +0300

    feat(ui): Add disabled state to solid and outlined buttons

--- src/shared/ui/buttons/button.tsx
+++ src/shared/ui/buttons/button.tsx
@@ -3,6 +3,7 @@ import { twMerge } from "tailwind-merge";
 
 type ButtonSize = "small" | "large";
 type ButtonVariant = "solid" | "outlined" | "outlinedRed";
+type VariantToClassNameFunction = (disabled: boolean) => string;
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   size?: ButtonSize;
@@ -15,30 +16,41 @@ const sizeToClassName: Record<ButtonSize, string> = {
   large: "py-3.5 px-5 text-[1rem]/[1.25] font-bold outline-2 -outline-offset-2",
 };
 
-const variantToClassName: Record<ButtonVariant, string> = {
-  solid: "text-crust outline-lavender bg-lavender active:bg-lavender-active",
-  outlined: twMerge(
-    "text-text outline outline-lavender",
-    "active:bg-lavender-active active:outline-lavender-active active:text-crust",
-  ),
-  outlinedRed: twMerge(
-    "text-text outline outline-red",
-    "active:bg-red-active active:outline-red-active active:text-crust",
-  ),
+const variantToClassName: Record<ButtonVariant, VariantToClassNameFunction> = {
+  solid: (disabled) =>
+    `text-crust bg-lavender ${
+      disabled ? "opacity-50" : "active:bg-lavender-active"
+    }`,
+  outlined: (disabled) =>
+    `text-text outline outline-lavender ${
+      disabled
+        ? "opacity-50"
+        : "active:bg-lavender-active active:outline-lavender-active active:text-crust"
+    }`,
+  outlinedRed: (disabled) =>
+    `text-text outline outline-red ${
+      disabled
+        ? "opacity-50"
+        : "active:bg-red-active active:outline-red-active active:text-crust"
+    }`,
 };
 
 export const Button = ({
   size = "large",
   variant = "solid",
+  disabled = false,
   children,
+  ...props
 }: ButtonProps) => {
   return (
     <button
       className={twMerge(
         "rounded transition-colors",
         sizeToClassName[size],
-        variantToClassName[variant],
+        variantToClassName[variant](disabled),
       )}
+      disabled={disabled}
+      {...props}
     >
       {children}
     </button>
