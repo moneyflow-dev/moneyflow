215630b1a34a0a2f7e7ec205b6e81a3de50c810b
astsu <astsu.dev@protonmail.com>
2023-04-22 11:38:54 +0300

    feat(ui)!: Add `outlinedOverlay1` button variant. Rename button sizes

--- src/shared/ui/buttons/button.tsx
+++ src/shared/ui/buttons/button.tsx
@@ -1,8 +1,8 @@
 import { ButtonHTMLAttributes } from "react";
 import { twMerge } from "tailwind-merge";
 
-type ButtonSize = "small" | "large";
-type ButtonVariant = "solid" | "outlined" | "outlinedRed";
+type ButtonSize = "sm" | "md" | "lg";
+type ButtonVariant = "solid" | "outlined" | "outlinedRed" | "outlinedOverlay1";
 type VariantToClassNameFunction = (disabled: boolean) => string;
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
@@ -11,8 +11,9 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 }
 
 const sizeToClassName: Record<ButtonSize, string> = {
-  small: "py-2 px-3 text-xsm font-bold outline-1 -outline-offset-1",
-  large: "py-3.5 px-5 text-base font-bold outline-2 -outline-offset-2",
+  sm: "py-2 px-3 text-xsm font-bold outline-1 -outline-offset-1",
+  md: "py-3 px-4 gap-2.5 text-sm font-bold outline-2 -outline-offset-2",
+  lg: "py-3.5 px-5 text-base-size font-bold outline-2 -outline-offset-2",
 };
 
 const variantToClassName: Record<ButtonVariant, VariantToClassNameFunction> = {
@@ -32,10 +33,16 @@ const variantToClassName: Record<ButtonVariant, VariantToClassNameFunction> = {
         ? "opacity-50"
         : "active:bg-red-active active:outline-red-active active:text-crust"
     }`,
+  outlinedOverlay1: (disabled) =>
+    `text-overlay1 outline outline-overlay1 ${
+      disabled
+        ? "opacity-50"
+        : "active:bg-overlay1 active:outline-overlay1 active:text-crust"
+    }`,
 };
 
 export const Button = ({
-  size = "large",
+  size = "lg",
   variant = "solid",
   disabled = false,
   className,
@@ -45,7 +52,7 @@ export const Button = ({
   return (
     <button
       className={twMerge(
-        "rounded transition-colors inline-flex justify-center",
+        "rounded transition-colors inline-flex items-center justify-center",
         sizeToClassName[size],
         variantToClassName[variant](disabled),
         className,
