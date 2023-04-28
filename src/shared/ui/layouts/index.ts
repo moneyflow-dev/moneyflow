c26b2f41fdc7b5c2a7aa2f97e03c972a406e5fad
astsu <astsu.dev@protonmail.com>
2023-04-28 19:48:14 +0300

    feat(ui): Add page layout component

---
+++ src/shared/ui/layouts/index.ts
@@ -0,0 +1,1 @@
+export { PageLayout } from "./page-layout";
---
+++ src/shared/ui/layouts/page-layout.tsx
@@ -0,0 +1,27 @@
+import { ReactNode } from "react";
+import { twMerge } from "tailwind-merge";
+
+interface PageLayoutProps {
+  hasHorizontalPaddings?: boolean;
+  hasBottomPadding?: boolean;
+  children?: ReactNode;
+  className?: string;
+}
+
+export const PageLayout = ({
+  children,
+  className,
+  hasHorizontalPaddings = true,
+}: PageLayoutProps) => {
+  return (
+    <div
+      className={twMerge(
+        "flex flex-col gap-3 overflow-y-auto",
+        hasHorizontalPaddings && "px-5",
+        className,
+      )}
+    >
+      {children}
+    </div>
+  );
+};
