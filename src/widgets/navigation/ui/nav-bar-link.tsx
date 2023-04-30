840496c597ce140f37081750fb9f870a5cbd2d5c
astsu <astsu.dev@protonmail.com>
2023-04-30 18:35:34 +0300

    feat(navigation): Add navbar widget

---
+++ src/widgets/navigation/ui/nav-bar-link.tsx
@@ -0,0 +1,19 @@
+import { twMerge } from "tailwind-merge";
+
+import { Link, LinkProps } from "@shared/ui/links";
+
+export const NavBarLink = ({ children, ...props }: LinkProps) => {
+  return (
+    <Link
+      className={({ isActive }) =>
+        twMerge(
+          "flex justify-center items-center py-10 cursor-default transition-colors",
+          isActive ? "text-lavender" : "text-overlay1 active:text-overlay2",
+        )
+      }
+      {...props}
+    >
+      {children}
+    </Link>
+  );
+};
---
+++ src/widgets/navigation/ui/nav-bar.tsx
@@ -0,0 +1,27 @@
+import {
+  ListIcon,
+  SettingsIcon,
+  StatisticsIcon,
+  WalletIcon,
+} from "@shared/ui/icons";
+
+import { NavBarLink } from "./nav-bar-link";
+
+export const NavBar = () => {
+  return (
+    <nav className="grid grid-cols-4 justify-center px-[7.61%]">
+      <NavBarLink to="/transactions">
+        <ListIcon size="lg" />
+      </NavBarLink>
+      <NavBarLink to="/statistics">
+        <StatisticsIcon size="lg" />
+      </NavBarLink>
+      <NavBarLink to="/balances">
+        <WalletIcon size="lg" />
+      </NavBarLink>
+      <NavBarLink to="/settings">
+        <SettingsIcon size="lg" />
+      </NavBarLink>
+    </nav>
+  );
+};
