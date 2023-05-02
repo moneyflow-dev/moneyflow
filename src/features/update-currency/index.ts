a293ea0a7ae6858c8037a580b380fb70da7771c5
astsu <astsu.dev@protonmail.com>
2023-05-02 10:32:16 +0300

    feat(currency): Add update currency feature

---
+++ src/features/update-currency/index.ts
@@ -0,0 +1,1 @@
+export { UpdateCurrencyButton } from "./ui/update-currency-button";
---
+++ src/features/update-currency/ui/update-currency-button.tsx
@@ -0,0 +1,44 @@
+import { useFormContext } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { twMerge } from "tailwind-merge";
+
+import {
+  CurrencyID,
+  UpdateCurrency,
+  useCurrenciesStore,
+} from "@entities/currency";
+
+import { CheckIcon } from "@shared/ui/icons";
+
+interface UpdateCurrencyButtonProps {
+  id: CurrencyID;
+  className?: string;
+}
+
+export const UpdateCurrencyButton = ({
+  id,
+  className,
+}: UpdateCurrencyButtonProps) => {
+  const { updateCurrency } = useCurrenciesStore((state) => ({
+    updateCurrency: state.updateCurrency,
+  }));
+  const navigate = useNavigate();
+  const { handleSubmit } = useFormContext<UpdateCurrency>();
+
+  const onUpdate = async (currency: UpdateCurrency) => {
+    await updateCurrency(id, currency);
+    navigate(-1);
+  };
+
+  return (
+    <button onClick={handleSubmit(onUpdate)}>
+      <CheckIcon
+        size="sm"
+        className={twMerge(
+          "text-overlay1 active:text-overlay2 transition-colors",
+          className,
+        )}
+      />
+    </button>
+  );
+};
