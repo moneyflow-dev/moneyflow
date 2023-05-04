450ac823662e4803672245170863b7dad8eda52d
astsu <astsu.dev@protonmail.com>
2023-05-04 20:56:20 +0300

    feat(currency): Add exporting types for currencies api

--- src/shared/api/currencies-api/index.ts
+++ src/shared/api/currencies-api/index.ts
@@ -1,4 +1,6 @@
 import { CurrenciesAPI } from "./currencies-api.interface";
 import { PreferencesCurrenciesAPI } from "./preferences-currencies-api";
 
+export { type CurrencyID } from "./dtos";
+
 export const currenciesApi: CurrenciesAPI = new PreferencesCurrenciesAPI();
