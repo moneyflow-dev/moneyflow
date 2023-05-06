8a6157af01f5e57f4e917372a56e3811f23f8aff
astsu <astsu.dev@protonmail.com>
2023-05-06 11:18:09 +0300

    feat(router): Connect income categories pages to router

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -2,11 +2,7 @@ import { Capacitor } from "@capacitor/core";
 import { StatusBar } from "@capacitor/status-bar";
 import { NavigationBar } from "@hugotomazi/capacitor-navigation-bar";
 import { useEffect } from "react";
-import {
-  Navigate,
-  RouterProvider,
-  createBrowserRouter,
-} from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import { AccountOverviewPage } from "@pages/account-overview";
 import { BalancesPage } from "@pages/balances";
@@ -14,8 +10,10 @@ import { CategoriesPage } from "@pages/categories";
 import { CreateAccountPage } from "@pages/create-account";
 import { CreateCurrencyPage } from "@pages/create-currency";
 import { CreateExpenseCategoryPage } from "@pages/create-expense-category";
+import { CreateIncomeCategoryPage } from "@pages/create-income-category";
 import { CurrencyOverviewPage } from "@pages/currency-overview";
 import { ExpenseCategoryOverviewPage } from "@pages/expense-category-overview";
+import { IncomeCategoryOverviewPage } from "@pages/income-category-overview";
 import { RootPage } from "@pages/root";
 import { RootLayoutPage } from "@pages/root-layout";
 import { SettingsPage } from "@pages/settings";
@@ -64,6 +62,14 @@ const router = createBrowserRouter([
   {
     path: "/expense-categories/:id",
     element: <ExpenseCategoryOverviewPage />,
+  },
+  {
+    path: "/income-categories/create",
+    element: <CreateIncomeCategoryPage />,
+  },
+  {
+    path: "/income-categories/:id",
+    element: <IncomeCategoryOverviewPage />,
   },
 ]);
 
