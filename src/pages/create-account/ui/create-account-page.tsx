2866683650d55698ab699f6c181478babf7b4062
astsu <astsu.dev@protonmail.com>
2023-05-08 09:43:17 +0300

    fix(account): Change header title on the create account page

--- src/pages/create-account/ui/create-account-page.tsx
+++ src/pages/create-account/ui/create-account-page.tsx
@@ -7,7 +7,7 @@ import { PageLayout } from "@shared/ui/layouts";
 export const CreateAccountPage = () => {
   return (
     <PageLayout className="h-full" hasBottomPadding>
-      <Header title="Create Account" backButton />
+      <Header title="Add Account" backButton />
       <CreateAccountForm className="flex-1" />
     </PageLayout>
   );
