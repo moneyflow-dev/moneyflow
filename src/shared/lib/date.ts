bd9f23d6635fece8be76d0f77a124e5b08827a43
astsu <astsu.dev@protonmail.com>
2023-05-09 16:35:30 +0300

    feat(lib): Add function for getting current local datetime

---
+++ src/shared/lib/date.ts
@@ -0,0 +1,5 @@
+export const getNowLocalDatetime = (): string => {
+  const now = new Date();
+  now.setMinutes(now.getMinutes() - now.getTimezoneOffset());
+  return now.toISOString().slice(0, 16);
+};
