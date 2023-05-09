14e20720d073b0334a953285033e72f95640b490
astsu <astsu.dev@protonmail.com>
2023-05-09 17:25:46 +0300

    fix(account): Remove bottom padding for create account page

--- src/pages/create-account/ui/create-account-page.tsx
+++ src/pages/create-account/ui/create-account-page.tsx
@@ -6,7 +6,7 @@ import { PageLayout } from "@shared/ui/layouts";
 
 export const CreateAccountPage = () => {
   return (
-    <PageLayout className="h-full" hasBottomPadding>
+    <PageLayout className="h-full">
       <Header title="Add Account" backButton />
       <CreateAccountForm className="flex-1" />
     </PageLayout>
