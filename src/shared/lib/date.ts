fdc9859ead896c4a275388b9f141c7340af5ac7e
astsu <astsu.dev@protonmail.com>
2023-05-10 08:56:09 +0300

    feat(lib): Add date utitilies

--- src/shared/lib/date.ts
+++ src/shared/lib/date.ts
@@ -1,5 +1,21 @@
+import { DateTime } from "luxon";
+
+export const toLocalDatetime = (datetime: DateTime): string => {
+  return datetime.toFormat("yyyy-LL-dd'T'T");
+};
+
 export const getNowLocalDatetime = (): string => {
-  const now = new Date();
-  now.setMinutes(now.getMinutes() - now.getTimezoneOffset());
-  return now.toISOString().slice(0, 16);
+  return toLocalDatetime(DateTime.now());
+};
+
+export const sameDay = (a: DateTime, b: DateTime): boolean => {
+  return a.hasSame(b, "year") && a.hasSame(b, "month") && a.hasSame(b, "day");
+};
+
+export const isToday = (datetime: DateTime): boolean => {
+  return sameDay(datetime, DateTime.now());
+};
+
+export const isYesterday = (datetime: DateTime): boolean => {
+  return sameDay(datetime, DateTime.now().minus({ day: 1 }));
 };
