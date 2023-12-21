5eeabf3af02efdffa085d7988940d25743cbc68f
Danshin Mikhail <effectdopler0@gmail.com>
2023-12-21 15:08:54 +0800

    feat(statisctics): Change the direction of CurrencyTotalCard for small devices

--- src/pages/statistics/ui/statistics-page.tsx
+++ src/pages/statistics/ui/statistics-page.tsx
@@ -133,7 +133,7 @@ export const StatisticsPage = () => {
                   {dateRangeString}
                 </span>
                 <div className="flex flex-col gap-6">
-                  <div className="grid grid-cols-2 gap-4">
+                  <div className="grid gap-4 sm:grid-flow-col">
                     <CurrencyTotalCard
                       type="expense"
                       currency={currency}
